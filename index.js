5c3e2f20a36b3821121b3542aafe7d7b8776cf24
Aiswaryagithub-10 <aiswarya.p.b10@gmail.com>
2024-11-12 23:19:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const express = require ("express")
+const cors = require("cors")
+const app = express()
+
+app.use(cors())
+var username = "Aishu"
+var password = "1025"
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.post("/login", function(req,res)
+{
+  console.log(req.body.username)
+  if(req.body.username=== username && req.body.password === password)
+  {
+    res.send(true)
+  }
+  else{
+    res.send (false)
+  }
+})
+app.listen(5003, function (){
+  console.log ("server started.....")
+})
